4c9dbfb67517dd20eb64f00e3ce550928725177d
Ziyue Zhou <ziyue5@illinois.edu>
2022-12-06 03:18:31 -0600

    Modify server file

--- CS410-Project/server.py
+++ CS410-Project/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from textblob import TextBlob
 import nltk
@@ -8,21 +8,22 @@ nltk.download('vader_lexicon')
 app = Flask(__name__)
 
 
-@app.route('/sentiment_score', methods=["GET", "POST"])
+@app.route('/sentiment_score', methods=["POST"])
 def sentiment_score():
-    if request == "POST":
-        inp = request.form.get("input")
+    inp = request.data.decode()
 
-        s = SentimentIntensityAnalyzer()
-        score1 = s.polarity_scores(inp)
+    s = SentimentIntensityAnalyzer()
+    score1 = s.polarity_scores(inp)
 
-        blob = TextBlob(inp)
-        score2 = blob.sentiment.polarity
+    blob = TextBlob(inp)
+    score2 = blob.sentiment.polarity
 
-        if score1['compound'] < 0 and score2 < 0:
-            return -1
-        elif score1['neg'] + score1['pos'] > 0 and score2 > 0:
-            return 1
-        else:
-            return 0
-    return 0
+    if score1['compound'] < 0 and score2 < 0:
+        return jsonify(-1)
+    elif score1['neg'] + score1['pos'] > 0 and score2 > 0:
+        return jsonify(1)
+    else:
+        return jsonify(0)
+
+if __name__ == '__main__':
+    app.run(port=8080)
